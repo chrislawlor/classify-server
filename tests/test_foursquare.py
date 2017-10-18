4a8749a330f35d95c7a6339bd17782f2cb8d23ff
ChatPion <chatpion@gmail.com>
2017-10-18 22:44:54 +0200

    Tested when there are no venue match

--- tests/test_foursquare.py
+++ tests/test_foursquare.py
@@ -16,6 +16,11 @@ async def test_match_venue_to_query_small_difference():
     result = foursquare.match_venue_to_query(query, venues)
     assert result == venues[0]
 
+async def test_match_venue_to_query_no_match():
+	venues = [{'name': 'foo'}]
+	query = 'bar'
+	result = foursquare.match_venue_to_query(query, venues)
+	assert result is None
 
 async def test_make_request():
     test_data = {'venues': []}
