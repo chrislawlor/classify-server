3aff8239f929490bf9342ee685d7b26a14c254dc
Jay Shepherd <shepherdjay@users.noreply.github.com>
2017-10-20 11:11:22 -0600

    enhanced MockClient for multiple response, resolves #3
    Initial crack at issue #3
    backwards compatibility with existing tests
    added tests for verifying mockclient extensions

--- tests/mocks.py
+++ tests/mocks.py
@@ -21,7 +21,14 @@ class MockResponse(object):
 
 class MockClient(object):
     def __init__(self, response_data, response_status=200):
-        self.response = MockResponse(response_data, response_status)
+        self.responses = {}
+        self.default_response = MockResponse(response_data, response_status)
 
-    def get(self, url):
-        return self.response
+    def get(self, url=None):
+        try:
+            return self.responses[url]
+        except KeyError:
+            return self.default_response
+
+    def add_response(self, url, response_data, response_status=200):
+        self.responses[url] = MockResponse(response_data, response_status)
